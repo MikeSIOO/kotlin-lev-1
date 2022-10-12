4e9d274d4d26f516b505405633f58c429059b242
MikeSIOO <m.pazhitnov@yandex.ru>
2022-10-12 11:42:03 +0300

    Сохранение состояния в фрагменте

--- homework-1/app/src/main/java/com/example/homework_1/MainActivity.kt
+++ homework-1/app/src/main/java/com/example/homework_1/MainActivity.kt
@@ -2,35 +2,11 @@ package com.example.homework_1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.PersistableBundle
 
-class MainActivity : AppCompatActivity(), OnButtonListener {
-    private val COUNT = "NumOfElements"
-    private var counter: Int = 0
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val fragmentManager = supportFragmentManager
-        val workFragment: WorkFragment? = fragmentManager.findFragmentById(R.id.fragment_work) as WorkFragment?
-        workFragment?.changeOrientation(resources.configuration.orientation)
-    }
-
+class MainActivity : AppCompatActivity(R.layout.activity_main), OnButtonListener {
     override fun onButton() {
-        counter++
         val fragmentManager = supportFragmentManager
         var workFragment: WorkFragment? = fragmentManager.findFragmentById(R.id.fragment_work) as WorkFragment?
-        workFragment?.createDie(counter)
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) { // Here You have to save count value
-        super.onSaveInstanceState(outState)
-        outState.putInt(COUNT, counter)
-    }
-
-    override fun onRestoreInstanceState(savedInstanceState: Bundle) { // Here You have to restore count value
-        super.onRestoreInstanceState(savedInstanceState)
-        counter = savedInstanceState.getInt(COUNT)
+        workFragment?.createDie()
     }
 }
--- homework-1/app/src/main/java/com/example/homework_1/WorkFragment.kt
+++ homework-1/app/src/main/java/com/example/homework_1/WorkFragment.kt
@@ -2,34 +2,54 @@ package com.example.homework_1
 
 import android.content.res.Configuration
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
 
 class WorkFragment : Fragment() {
     private lateinit var textView: TextView
+//    private lateinit var gridLayoutManager: GridLayoutManager
+
+    private val COUNT = "NumOfElements"
+    private var counter: Int = 0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+        if (resources.configuration.orientation == Configuration.ORIENTATION_PORTRAIT) {
+            // TODO вертикальная ориентация
+//            gridLayoutManager.setLayoutManager(GridLayoutManager(context,3))
+        } else {
+            // TODO горизонтальная ориентация
+//            gridLayoutManager.setLayoutManager(GridLayoutManager(context, 4))
+        }
         return inflater.inflate(R.layout.fragment_work, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        textView = view.findViewById(R.id.text)
+        textView = view.findViewById(R.id.textView)
     }
 
-    fun createDie(counter: Int) {
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(COUNT, counter)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        if (savedInstanceState != null) {
+            counter = savedInstanceState.getInt(COUNT)
+        }
         textView.text = counter.toString()
-        // TODO сделать добавление плашек
     }
 
-    fun changeOrientation(orient: Int) {
-        textView.text = if (orient == Configuration.ORIENTATION_PORTRAIT) "Port" else "Land"
+    fun createDie() {
+        counter++
+        textView.text = counter.toString() // TODO сделать добавление плашек
     }
 }
