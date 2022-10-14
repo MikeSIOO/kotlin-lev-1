2e2c4d973ed18f7849f4029170a9654fed5d4be4
MikeSIOO <m.pazhitnov@yandex.ru>
2022-10-14 12:59:52 +0300

    Вынес адаптер в отдельный класс

---
+++ homework-1/app/src/main/java/com/example/homework_1/MyAdapter.kt
@@ -0,0 +1,45 @@
+package com.example.homework_1
+
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.recyclerview.widget.RecyclerView
+
+class MyAdapter(
+    private val counter: Int,
+) : RecyclerView.Adapter<MyAdapter.MyHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.die, null)
+        return MyHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: MyHolder, position: Int) {
+        holder.textView.text = (position+1).toString()
+        holder.background.setBackgroundColor(
+            // TODO сделать загрузку цветов из ресурсов
+            if (position % 2 != 0) Color.RED else Color.BLUE)
+//        holder.bind(dies[position])
+    }
+
+    override fun getItemCount(): Int {
+        return counter
+    }
+
+    class MyHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val textView: TextView = itemView.findViewById(R.id.textView)
+        val background: ConstraintLayout = itemView.findViewById(R.id.background)
+
+        init {}
+
+        fun bind(die: Die) {
+            textView.text = die.text
+            background.setBackgroundColor(
+                // TODO сделать загрузку цветов из ресурсов
+                if (position % 2 != 0) Color.RED else Color.BLUE)
+//                .setBackgroundColor(ContextCompat.getColor(this, R.color.white))
+        }
+    }
+}
--- homework-1/app/src/main/java/com/example/homework_1/WorkFragment.kt
+++ homework-1/app/src/main/java/com/example/homework_1/WorkFragment.kt
@@ -1,23 +1,20 @@
 package com.example.homework_1
 
 import android.content.res.Configuration
-import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class WorkFragment : Fragment() {
     private val NUM_OF_ELEMENTS = "NumOfElements"
-    private var counter: Int = 1
+    private var counter: Int = 4
 
     private lateinit var recyclerView: RecyclerView
-    private lateinit var customRecyclerAdapter: CustomRecyclerAdapter
+    private lateinit var myAdapter: MyAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,8 +29,8 @@ class WorkFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         recyclerView = view.findViewById(R.id.recyclerView)
-        customRecyclerAdapter = CustomRecyclerAdapter(counter)
-        recyclerView.adapter = customRecyclerAdapter
+        myAdapter = MyAdapter(counter)
+        recyclerView.adapter = myAdapter
         if (resources.configuration.orientation == Configuration.ORIENTATION_PORTRAIT) {
             recyclerView.layoutManager = GridLayoutManager(view.context, 3)
         } else {
@@ -44,30 +41,6 @@ class WorkFragment : Fragment() {
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
         outState.putInt(NUM_OF_ELEMENTS, counter)
-    }
-
-    class CustomRecyclerAdapter(private val count: Int) : RecyclerView.Adapter<CustomRecyclerAdapter.MyViewHolder>() {
-        class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
-            val textView: TextView = itemView.findViewById(R.id.textView)
-            val die: ConstraintLayout = itemView.findViewById(R.id.die)
-        }
-
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-            val itemView = LayoutInflater.from(parent.context).inflate(R.layout.die, parent, false)
-            return MyViewHolder(itemView)
-        }
-
-        override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-            holder.textView.text = (position+1).toString()
-            holder.die.setBackgroundColor(
-                // TODO сделать загрузку цветов из ресурсов
-                if (position % 2 != 0) Color.RED else Color.BLUE)
-//                .setBackgroundColor(ContextCompat.getColor(this, R.color.white))
-        }
-
-        override fun getItemCount(): Int {
-            return count
-        }
     }
 
     fun createDie() {
