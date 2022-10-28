af0b84d7474047038df27c2962cd72fe81b0d56e
MikeSIOO <m.pazhitnov@yandex.ru>
2022-10-28 21:03:54 +0300

    Вынес холдер в отдельный класс

--- homework-1/app/src/main/java/com/example/homework_1/MyAdapter.kt
+++ homework-1/app/src/main/java/com/example/homework_1/MyAdapter.kt
@@ -1,16 +1,12 @@
 package com.example.homework_1
 
-import android.graphics.Color
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 
 class MyAdapter(
-    private val dies: ArrayList<Die>?,
-) : RecyclerView.Adapter<MyAdapter.MyHolder>() {
+    private val dies: ArrayList<Die>,
+) : RecyclerView.Adapter<MyHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyHolder {
         val itemView = LayoutInflater.from(parent.context).inflate(R.layout.die, null)
         return MyHolder(itemView)
@@ -18,15 +14,10 @@ class MyAdapter(
 
     override fun onBindViewHolder(holder: MyHolder, position: Int) {
         holder.textView.text = (position + 1).toString()
-        holder.background.setBackgroundColor(dies?.get(position)?.color ?: Color.BLACK)
+        holder.background.setBackgroundColor(dies[position].color)
     }
 
     override fun getItemCount(): Int {
-        return dies?.size ?: 0
-    }
-
-    class MyHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val textView: TextView = itemView.findViewById(R.id.textView)
-        val background: ConstraintLayout = itemView.findViewById(R.id.background)
+        return dies.size
     }
 }
