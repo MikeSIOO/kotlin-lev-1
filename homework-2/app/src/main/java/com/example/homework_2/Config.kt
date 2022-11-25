6a5bf3fb52ac2131f65f053e62ccb4f0228cfb73
MikeSIOO <m.pazhitnov@yandex.ru>
2022-11-25 20:57:54 +0300

    Мелкие правки

--- homework-2/app/src/main/java/com/example/homework_2/Config.kt
+++ homework-2/app/src/main/java/com/example/homework_2/Config.kt
@@ -9,7 +9,7 @@ enum class StatusLoad {
 enum class Response(val value: String) {
     API_KEY("9FF4Dz6pMk2JDlphgZOMSOU4l4fBWNII"),
     Q("cat"),
-    LIMIT("10"),
+    LIMIT("3"),
     RATING("g"),
     LANG("en"),
 }
