d6478d5c9d45ea3c37f72ed20ad52d9d7a5c4642
MikeSIOO <m.pazhitnov@yandex.ru>
2022-12-09 17:01:43 +0300

    Navigation

--- homework-3/app/src/main/java/com/example/homework_3/MainActivity.kt
+++ homework-3/app/src/main/java/com/example/homework_3/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.homework_3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar!!.hide()
+
+        val button_product = findViewById<Button>(R.id.button_product)
+        val button_search = findViewById<Button>(R.id.button_search)
+        val button_profile = findViewById<Button>(R.id.button_profile)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction().add(R.id.fragment_main, ProductFragment()).commit()
+        }
+
+        button_product.setOnClickListener {
+            supportFragmentManager.beginTransaction().replace(R.id.fragment_main, ProductFragment()).commit()
+        }
+        button_search.setOnClickListener {
+            supportFragmentManager.beginTransaction().replace(R.id.fragment_main, SearchFragment()).commit()
+        }
+        button_profile.setOnClickListener {
+            supportFragmentManager.beginTransaction().replace(R.id.fragment_main, ProfileFragment()).commit()
+        }
     }
 }
