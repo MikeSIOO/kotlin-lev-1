93b77dfd5bec82b62e917ba85817b862b2bff2f5
MikeSIOO <m.pazhitnov@yandex.ru>
2022-12-21 15:48:59 +0300

    Кликабельные все кнопки на главной

--- homework-3/app/src/main/java/com/example/homework_3/SearchFragment.kt
+++ homework-3/app/src/main/java/com/example/homework_3/SearchFragment.kt
@@ -18,43 +18,69 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 class SearchFragment : Fragment(R.layout.fragment_search) {
+    private lateinit var stub: TextView
+    private lateinit var load: ProgressBar
+    private lateinit var card: CardView
+    private lateinit var image: ImageView
+    private lateinit var tags: TextView
+    private lateinit var name: TextView
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val stub = view.findViewById<TextView>(R.id.stub)
-        val load = view.findViewById<ProgressBar>(R.id.load)
-        val card = view.findViewById<CardView>(R.id.card)
+        stub = view.findViewById(R.id.stub)
+        load = view.findViewById(R.id.load)
+        card = view.findViewById(R.id.card)
+        image = view.findViewById(R.id.image)
+        tags = view.findViewById(R.id.tags)
+        name = view.findViewById(R.id.name)
+
+        var id: Int = 0
+        loadData(id)
+
+        view.findViewById<Button>(R.id.likeButton).setOnClickListener {
+            findNavController().navigate(R.id.action_searchFragment_to_recipeFragment)
+        }
+
+        view.findViewById<Button>(R.id.filterButton).setOnClickListener {
+            findNavController().navigate(R.id.action_searchFragment_to_filterFragment)
+        }
+
+        view.findViewById<Button>(R.id.dislikeButton).setOnClickListener {
+            loadData(++id)
+        }
+
+        view.findViewById<Button>(R.id.returnButton).setOnClickListener {
+            loadData(--id)
+        }
+
+        stub.setOnClickListener {
+            loadData(id)
+        }
+    }
+
+    private fun loadData(id: Int) {
+        println(id)
         lifecycleScope.launch {
             load.isVisible = true
             stub.isVisible = false
             card.isVisible = false
             try {
                 val data = withContext(Dispatchers.IO) {
-                    SearchResult.getSearchById(1)
+                    SearchResult.getSearchById(id)
                 }
 
                 load.isVisible = false
                 stub.isVisible = false
                 card.isVisible = true
-                Glide
-                    .with(requireContext())
-                    .load(data.image)
-                    .into(view.findViewById<ImageView>(R.id.image))
-                view.findViewById<TextView>(R.id.tags).text = data.tags.joinToString("\n")
-                view.findViewById<TextView>(R.id.name).text = "${data.name}, ${data.time}"
+                Glide.with(requireContext()).load(data.image).into(image)
+                tags.text = data.tags.joinToString("\n")
+                name.text = "${data.name}, ${data.time}"
             } catch (error: Throwable) {
                 load.isVisible = false
                 stub.isVisible = true
                 card.isVisible = false
             }
-        }
-
-        view.findViewById<Button>(R.id.likeButton).setOnClickListener{
-            findNavController().navigate(R.id.action_searchFragment_to_recipeFragment)
-        }
-
-        view.findViewById<Button>(R.id.filterButton).setOnClickListener{
-            findNavController().navigate(R.id.action_searchFragment_to_filterFragment)
         }
     }
 }
