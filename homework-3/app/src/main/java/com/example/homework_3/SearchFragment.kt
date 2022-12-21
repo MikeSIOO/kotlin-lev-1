4cb6d8834b97436c900715147564d2ac1d018d2e
MikeSIOO <m.pazhitnov@yandex.ru>
2022-12-21 15:25:38 +0300

    главная страница

--- homework-3/app/src/main/java/com/example/homework_3/SearchFragment.kt
+++ homework-3/app/src/main/java/com/example/homework_3/SearchFragment.kt
@@ -1,9 +1,14 @@
 package com.example.homework_3
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
@@ -13,25 +18,34 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 class SearchFragment : Fragment(R.layout.fragment_search) {
-    lateinit var image: ImageView
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        image = view.findViewById(R.id.image)
-
+        val stub = view.findViewById<TextView>(R.id.stub)
+        val load = view.findViewById<ProgressBar>(R.id.load)
+        val card = view.findViewById<CardView>(R.id.card)
         lifecycleScope.launch {
-            // TODO loading
+            load.isVisible = true
+            stub.isVisible = false
+            card.isVisible = false
             try {
-                val request = withContext(Dispatchers.IO) {
-                    SearchResult.getSearchById(0)
+                val data = withContext(Dispatchers.IO) {
+                    SearchResult.getSearchById(1)
                 }
+
+                load.isVisible = false
+                stub.isVisible = false
+                card.isVisible = true
                 Glide
                     .with(requireContext())
-                    .load(request.image)
-                    .into(image)
+                    .load(data.image)
+                    .into(view.findViewById<ImageView>(R.id.image))
+                view.findViewById<TextView>(R.id.tags).text = data.tags.joinToString("\n")
+                view.findViewById<TextView>(R.id.name).text = "${data.name}, ${data.time}"
             } catch (error: Throwable) {
-                // TODO error
+                load.isVisible = false
+                stub.isVisible = true
+                card.isVisible = false
             }
         }
 
