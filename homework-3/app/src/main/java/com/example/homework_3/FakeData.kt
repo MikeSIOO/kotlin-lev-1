c77ca97f2ef4829241518b316a6b5d3d222162e1
MikeSIOO <m.pazhitnov@yandex.ru>
2022-12-21 13:19:31 +0300

    Фейковые данные

---
+++ homework-3/app/src/main/java/com/example/homework_3/FakeData.kt
@@ -0,0 +1,156 @@
+package com.example.homework_3
+
+import kotlinx.coroutines.delay
+
+class SearchResult(val searchElements: ArrayList<SearchElement> = arrayListOf()) {
+    class SearchElement(
+        val name: String,
+        val time: String,
+        val image: Int,
+        val tags: ArrayList<String>
+    )
+
+    companion object {
+        private val searchResult: ArrayList<SearchElement> = arrayListOf(
+            SearchElement(
+                "Омлет",
+                "30 минут",
+                R.drawable.first,
+                arrayListOf("Завтрак", "Легко приготовить")
+            ),
+            SearchElement(
+                "Омлет получше",
+                "20 минут",
+                R.drawable.second,
+                arrayListOf("Обед", "Легко приготовить", "Веганский")
+            ),
+            SearchElement(
+                "Омлет мечты",
+                "10 минут",
+                R.drawable.third,
+                arrayListOf("Перекус", "Легко приготовить")
+            )
+        )
+
+        suspend fun getSearchById(id: Int): SearchElement {
+            delay(2000L)
+            return searchResult[id % 3]
+        }
+    }
+}
+
+class FullRecipe(val recipeElements: ArrayList<RecipeElement>) {
+    class RecipeElement(
+        val name: String,
+        val time: String,
+        val image: Int,
+        val ingredients: ArrayList<Ingredients>,
+        val steps: ArrayList<Step>
+    ) {
+        class Ingredients(
+            val name: String,
+            val quantity: String
+        )
+
+        class Step(
+            val id: Int,
+            val image: Int,
+            val description: String
+        )
+    }
+
+    companion object {
+        private val recipes: ArrayList<RecipeElement> = arrayListOf(
+            RecipeElement(
+                "Омлет",
+                "30 минут",
+                R.drawable.first,
+                arrayListOf(
+                    RecipeElement.Ingredients("Яйца", "2 шт"),
+                    RecipeElement.Ingredients("Помидор", "1 шт"),
+                    RecipeElement.Ingredients("Молоко", "100 мл"),
+                ),
+                arrayListOf(
+                    RecipeElement.Step(1, R.drawable.first, "Нужно сделать то-то и то-то, а потом еще вот это"),
+                    RecipeElement.Step(2, R.drawable.second, "Нужно сделать то-то и то-то, а потом еще вот это"),
+                    RecipeElement.Step(3, R.drawable.third, "Нужно сделать то-то и то-то, а потом еще вот это"),
+                    RecipeElement.Step(4, R.drawable.first, "Нужно сделать то-то и то-то, а потом еще вот это")
+                )
+            ),
+            RecipeElement(
+                "Омлет получше",
+                "20 минут",
+                R.drawable.first,
+                arrayListOf(
+                    RecipeElement.Ingredients("Яйца", "2 шт"),
+                    RecipeElement.Ingredients("Помидор", "1 шт"),
+                    RecipeElement.Ingredients("Молоко", "100 мл"),
+                ),
+                arrayListOf(
+                    RecipeElement.Step(1, R.drawable.first, "Нужно сделать то-то и то-то, а потом еще вот это"),
+                    RecipeElement.Step(2, R.drawable.second, "Нужно сделать то-то и то-то, а потом еще вот это"),
+                    RecipeElement.Step(3, R.drawable.third, "Нужно сделать то-то и то-то, а потом еще вот это"),
+                    RecipeElement.Step(4, R.drawable.first, "Нужно сделать то-то и то-то, а потом еще вот это")
+                )
+            ),
+            RecipeElement(
+                "Омлет мечты",
+                "10 минут",
+                R.drawable.first,
+                arrayListOf(
+                    RecipeElement.Ingredients("Яйца", "2 шт"),
+                    RecipeElement.Ingredients("Помидор", "1 шт"),
+                    RecipeElement.Ingredients("Молоко", "100 мл"),
+                ),
+                arrayListOf(
+                    RecipeElement.Step(1, R.drawable.first, "Нужно сделать то-то и то-то, а потом еще вот это"),
+                    RecipeElement.Step(2, R.drawable.second, "Нужно сделать то-то и то-то, а потом еще вот это"),
+                    RecipeElement.Step(3, R.drawable.third, "Нужно сделать то-то и то-то, а потом еще вот это"),
+                    RecipeElement.Step(4, R.drawable.first, "Нужно сделать то-то и то-то, а потом еще вот это")
+                )
+            ),
+        )
+
+        suspend fun getRecipe(id: Int): RecipeElement {
+            delay(2000L)
+            return recipes[id]
+        }
+    }
+}
+
+class ProductCategories(val categories: ArrayList<Category>) {
+    class Category(
+        val name: String,
+        val categoryElements: ArrayList<String>
+    )
+
+    companion object {
+        private val products: ArrayList<Category> = arrayListOf(
+            Category(
+                "Молоко, молочные продукты",
+                arrayListOf("Молоко", "Сливки", "Кефир", "Йогурт", "Творог", "Сметана", "Сливочное масло", "Сгущенка")
+            ),
+            Category(
+                "Мясо",
+                arrayListOf("Говядина", "Свинина", "Крольчатина", "Курица", "Индейка", "Утка", "Растительное мясо")
+            ),
+            Category(
+                "Сыры",
+                arrayListOf("Пармезан", "Сливочный", "Российский", "Легкий", "Страчателла", "Бри", "Моцарелла")
+            ),
+            Category(
+                "Мясная гастрономия",
+                arrayListOf("Вареная", "Докторская", "Охотничья", "Ветчина", "Сосиски", "Сардельки", "Бекон")
+            ),
+            Category(
+                "Овощи и фрукт",
+                arrayListOf("Помидоры", "Огурцы", "Сладкий перец", "Перец чили", "Авокадо", "Бананы", "Яблоки")
+            ),
+        )
+
+        suspend fun getProducts(): ArrayList<Category> {
+            delay(2000L)
+            return products
+        }
+    }
+}
--- homework-3/app/src/main/java/com/example/homework_3/SearchFragment.kt
+++ homework-3/app/src/main/java/com/example/homework_3/SearchFragment.kt
@@ -3,13 +3,37 @@ package com.example.homework_3
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class SearchFragment : Fragment(R.layout.fragment_search) {
+    lateinit var image: ImageView
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        image = view.findViewById(R.id.image)
+
+        lifecycleScope.launch {
+            // TODO loading
+            try {
+                val request = withContext(Dispatchers.IO) {
+                    SearchResult.getSearchById(0)
+                }
+                Glide
+                    .with(requireContext())
+                    .load(request.image)
+                    .into(image)
+            } catch (error: Throwable) {
+                // TODO error
+            }
+        }
 
         view.findViewById<Button>(R.id.likeButton).setOnClickListener{
             findNavController().navigate(R.id.action_searchFragment_to_recipeFragment)
